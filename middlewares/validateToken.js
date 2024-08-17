0e03594a304535986eb8e219f14056c1c07a65e4
Ali Aghayev <aliaghayev@Alis-Laptop.local>
2024-08-18 01:33:04 +0400

    V2.0

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -3,16 +3,16 @@ const validateAccessToken = (req, res, next) => {
   try {
     const authHeader = req.headers["authorization"];
     if (!authHeader)
-      return res.status(401).json({ error: "No token provided" });
+      return res.status(401).json({ message: "No token provided" });
     const token = authHeader.split(" ")[1];
-    if (!token) return res.status(401).json({ error: "Token missing" });
+    if (!token) return res.status(401).json({ message: "Token missing" });
     const decoded = jwt.verify(token, process.env.ACCESS_SECRET_KEY);
     delete decoded.password;
     req.user = decoded;
     next();
   } catch (error) {
     console.log(error);
-    return res.status(401).json({ error: "Invalid or expired token" });
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
