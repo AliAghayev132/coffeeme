bf16e64ff551f1fd24af4dc7db2aae21eb131f6e
Ali <aliaghayev132@gmail.com>
2024-08-29 12:46:21 +0400

    Partner Edit Route added

--- routes/admin/partnerRoute.js
+++ routes/admin/partnerRoute.js
@@ -3,6 +3,7 @@ const Partner = require("../../schemas/Partner");
 const router = express.Router();
 const validateAccessToken = require("../../middlewares/validateToken");
 
+// Bütün partnyorları qaytarır
 router.get("/", validateAccessToken, async (req, res) => {
   try {
     const partners = await Partner.find({}).lean();
@@ -12,7 +13,7 @@ router.get("/", validateAccessToken, async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
-
+// Verilmiş aralığdaki partnyorları qaytarır
 router.get("/next", validateAccessToken, async (req, res) => {
   try {
     const { offset, limit } = req.query;
@@ -21,6 +22,30 @@ router.get("/next", validateAccessToken, async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Verilmiş idili edit edit
+router.put("/:id", validateAccessToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, password } = req.body;
+    const partner = await Partner.findById(id);
+    if (!partner) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Partner not found" });
+    }
+
+    partner.username = username;
+    partner.password = password;
+
+    await partner.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Partner edited successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
