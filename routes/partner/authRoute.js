77c7429927bfe2e7fcc4a8d56b7999c53dbfff44
Ali <aliaghayev132@gmail.com>
2024-08-30 03:31:57 +0400

    Fixes

--- routes/partner/authRoute.js
+++ routes/partner/authRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Partner = require("../../schemas/Partner");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/login", async (req, res) => {
   try {
