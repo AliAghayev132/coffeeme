dc37c0ce47e5be7b111e213ab967dfaa270dba0b
Ali <aliaghayev132@gmail.com>
2024-08-30 03:01:20 +0400

    File paths fixed

--- routes/user/orderRoute.js
+++ routes/user/orderRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../schemas/Order"); // Order model
-const Product = require("../schemas/Product"); // Product model
-const User = require("../schemas/User"); // User model
-const Shop = require("../schemas/Shop");
-const validateAccessToken = require("../middlewares/validateToken");
+const Order = require("../../schemas/Order"); // Order model
+const Product = require("../../schemas/Product"); // Product model
+const User = require("../../schemas/User"); // User model
+const Shop = require("../../schemas/Shop");
+const validateAccessToken = require("../../middlewares/validateToken");
 
 router.get("/", validateAccessToken, async (req, res) => {
   try {
