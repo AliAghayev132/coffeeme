bad993ad30430c9eae917d27cc7e71f5c03e16f9
Ali <aliaghayev132@gmail.com>
2024-09-01 09:13:29 +0400

    Validation Fixes

--- routes/partner/authRoute.js
+++ routes/partner/authRoute.js
@@ -3,6 +3,7 @@ const Partner = require("../../schemas/Partner");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const validateAccessToken = require("../../middlewares/validateToken");
 
 router.post("/login", async (req, res) => {
   try {
@@ -40,11 +41,34 @@ router.post("/login", async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 });
-// router.post("/refresh-token", async (req, res) => {
-//   try{
+router.get("/", validateAccessToken, async (req, res) => {
+  try {
+    const partner = req.body.user;
+    console.log(partner);
+    if (!partner) {
+      return res.status(404).json({ message: "Partner not found" });
+    }
+    return res.status(200).json({ success: true, partner });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+router.post("/refresh-token", async (req, res) => {
+  try {
+    const { token } = req.body;
+    if (!token) return res.status(401).json({ error: "Unauthorized" });
+    const decoded = jwt.verify(token, process.env.REFRESH_SECRET_KEY);
+    const { email } = decoded;
 
-//   }catch(error){
-//     console.log(error);
-//   }
-// });
+    // Yeni access token oluştur
+    const newToken = jwt.sign({ email }, process.env.ACCESS_SECRET_KEY, {
+      expiresIn: "10m",
+    });
+    return res.status(200).json({ accessToken: newToken });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+});
 module.exports = router;
